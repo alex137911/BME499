66c59dc663afda7f48ec8ffd536eb53e086520a5
Alex Chan <alex.chan1@uwaterloo.ca>
2023-10-18 22:15:09 -0400

    Script to download SRA metagenome samples

--- download_sra.sh
+++ download_sra.sh
@@ -3,12 +3,16 @@
 #SBATCH --output=download_sra.out
 #SBATCH --error=download_sra.err
 #SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=32G
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem-per-cpu=8G  # Adjust this as necessary
 
-module load sra-toolkit
+# Load necessary modules
+module load StdEnv/2020
+module load gcc/9.3.0
+module load sra-toolkit/2.10.8
 
 # Path to the .sra file
 SRA_PATH="/home/a252chan/projects/def-acdoxey/a252chan/SRR15197167/SRR15197167.sra"
 
-fasterq-dump $SRA_PATH -e 32 -O /home/a252chan/projects/def-acdoxey/a252chan/SRR15197167/fastq
+fasterq-dump $SRA_PATH -e 16 -O /home/a252chan/projects/def-acdoxey/a252chan/SRR15197167/fastq
