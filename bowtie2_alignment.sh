ab1227bc04c650476e836232996497ba31bfdc54
Alex Chan <alex.chan1@uwaterloo.ca>
2023-11-29 12:34:53 -0500

    update (11/29/2023)

--- bowtie2_alignment.sh
+++ bowtie2_alignment.sh
@@ -21,31 +21,43 @@ for SRA_ID_DIR in $SRA_DIR/*/; do
     # Extract the SRA ID from the directory name
     SRA_ID=$(basename $SRA_ID_DIR)
 
-    # Specify the path to the trimmed FASTQ files
+     # Specify the path to the trimmed FASTQ files
     FASTQ_PATH="${SRA_ID_DIR}fastq"
+
+    # Define BAM file path
+    BAM_FILE="${SRA_ID_DIR}aligned_${SRA_ID}_reads.bam"
 
     # Check if trimmed FASTQ files exist
     if [[ -f "${FASTQ_PATH}/trimmed_${SRA_ID}_1.fastq" && -f "${FASTQ_PATH}/trimmed_${SRA_ID}_2.fastq" ]]; then
-        # Align reads using Bowtie2
-        bowtie2 -x /scratch/a252chan/closDifficile/FN545816 \
-        -1 "${FASTQ_PATH}/trimmed_${SRA_ID}_1.fastq" \
-        -2 "${FASTQ_PATH}/trimmed_${SRA_ID}_2.fastq" \
-        | samtools view -bS - > "${SRA_ID_DIR}aligned_${SRA_ID}_reads.bam"
-        
+
+        # Check if the BAM file exists (from previous runs)
+        if [[ -f "$BAM_FILE" ]]; then
+            # Check for EOF marker in BAM file
+            if samtools quickcheck "$BAM_FILE"; then
+                echo "BAM file for $SRA_ID is complete, skipping."
+                continue
+            else
+                echo "BAM file for $SRA_ID is corrupted or incomplete, rerunning alignment."
+                
+                # Align reads using Bowtie2
+                bowtie2 -x /scratch/a252chan/closDifficile/FN545816 \
+                -1 "${FASTQ_PATH}/trimmed_${SRA_ID}_1.fastq" \
+                -2 "${FASTQ_PATH}/trimmed_${SRA_ID}_2.fastq" \
+                | samtools view -bS - > "${SRA_ID_DIR}aligned_${SRA_ID}_reads.bam"
+            fi
+        else
+            echo "BAM file for $SRA_ID not found, starting alignment."
+            
+            # Align reads using Bowtie2
+            bowtie2 -x /scratch/a252chan/closDifficile/FN545816 \
+            -1 "${FASTQ_PATH}/trimmed_${SRA_ID}_1.fastq" \
+            -2 "${FASTQ_PATH}/trimmed_${SRA_ID}_2.fastq" \
+            | samtools view -bS - > "${SRA_ID_DIR}aligned_${SRA_ID}_reads.bam"
+        fi
+
         # Remove untrimmed FASTQ files
         find "${FASTQ_PATH}" -type f -name '*.fastq' ! -name '*trimmed*' -exec rm {} +
-    
     else
         echo "Trimmed FASTQ files for $SRA_ID not found."
     fi
 done
-
-
-# # Define the output directory
-# OUTPUT_DIR="/scratch/a252chan/SRR13622898"
-
-# # Align reads using Bowtie2
-# bowtie2 -x /scratch/a252chan/closDifficile/FN545816 \
-# -1 /scratch/a252chan/SRR13622898/fastq/trimmed_SRR13622898_1.fastq \
-# -2 /scratch/a252chan/SRR13622898/fastq/trimmed_SRR13622898_2.fastq \
-# | samtools view -bS - > $OUTPUT_DIR/aligned_SRR13622898reads.bam
