7eecae20a1a8d66ecf6addcb279d1e7a4f741455
Alex Chan <alex.chan1@uwaterloo.ca>
2023-11-29 12:35:11 -0500

    update (11/29/2023)

--- extracted_alignment.sh
+++ extracted_alignment.sh
@@ -24,8 +24,10 @@ REF_GENOME="/home/a252chan/scratch/closDifficile.fasta"
 CSV_FILE="${SRA_DIR}/mutation_analysis.csv"
 
 # Check if CSV file exists and add header if it doesn't
+# gyrA mutation/epidemic strain (6310), rpoB mutation/fidaxomicin (97336), 
+# nimB mutation/metronidazole (1547984), vanR mutation/vancomycin (1794733)
 if [ ! -f "$CSV_FILE" ]; then
-    echo "SRA_ID,gyrA_mutation,gyrA_coverage,nimB_mutation,nimB_coverage" > "$CSV_FILE"
+    echo "SRA_ID,gyrA_mutation,gyrA_coverage,rpoB_mutation,rpoB_coverage,nimB_mutation,nimB_coverage,vanR_mutation,vanR_coverage" > "$CSV_FILE"
 fi
 
 # Loop through each SRA ID directory
@@ -43,7 +45,7 @@ for SRA_ID_DIR in $SRA_DIR/*/; do
         rm -f "${OUTPUT_DIR}aligned_${SRA_ID}_reads_sorted.bam.tmp.*"
 
         # Check for EOF marker in BAM file (indicative of a complete file)
-        if ! samtools quickcheck "${OUTPUT_DIR}aligned_${SRA_ID}_reads.bam" || true; then
+        if ! samtools quickcheck "${OUTPUT_DIR}aligned_${SRA_ID}_reads.bam"; then
             echo "BAM file for $SRA_ID is corrupted or incomplete, skipping." >> "${SRA_DIR}/error_log.txt"
             continue
         fi
@@ -67,7 +69,9 @@ for SRA_ID_DIR in $SRA_DIR/*/; do
 
         # Check average coverage at relevant positions and extract variants
         coverage_at_gyrA=$(samtools depth -r FN545816.1:6310 "${OUTPUT_DIR}aligned_${SRA_ID}_reads_sorted.bam" | awk '{sum+=$3; cnt++} END {if (cnt>0) print sum/cnt; else print 0}')
+        coverage_at_rpoB=$(samtools depth -r FN545816.1:97336 "${OUTPUT_DIR}aligned_${SRA_ID}_reads_sorted.bam" | awk '{sum+=$3; cnt++} END {if (cnt>0) print sum/cnt; else print 0}')
         coverage_at_nimB=$(samtools depth -r FN545816.1:1547984 "${OUTPUT_DIR}aligned_${SRA_ID}_reads_sorted.bam" | awk '{sum+=$3; cnt++} END {if (cnt>0) print sum/cnt; else print 0}')
+        coverage_at_vanR=$(samtools depth -r FN545816.1:1794733 "${OUTPUT_DIR}aligned_${SRA_ID}_reads_sorted.bam" | awk '{sum+=$3; cnt++} END {if (cnt>0) print sum/cnt; else print 0}')
 
         # Checks if there is no coverage at all or if it is 0
         if [[ -z "$coverage_at_gyrA" || "$coverage_at_gyrA" == "0" ]]; then
@@ -76,14 +80,26 @@ for SRA_ID_DIR in $SRA_DIR/*/; do
             gyrA_base=$(bcftools query -f '%ALT' -r FN545816.1:6310 "${OUTPUT_DIR}${SRA_ID}_variants.vcf.gz")
         fi
 
+        if [[ -z "$coverage_at_rpoB" || "$coverage_at_rpoB" == "0" ]]; then
+            rpoB_base="Not Covered"
+        else
+            rpoB_base=$(bcftools query -f '%ALT' -r FN545816.1:97336 "${OUTPUT_DIR}${SRA_ID}_variants.vcf.gz")
+        fi
+
         if [[ -z "$coverage_at_nimB" || "$coverage_at_nimB" == "0" ]]; then
             nimB_base="Not Covered"
         else
             nimB_base=$(bcftools query -f '%ALT' -r FN545816.1:1547984 "${OUTPUT_DIR}${SRA_ID}_variants.vcf.gz")
         fi
 
+        if [[ -z "$coverage_at_vanR" || "$coverage_at_vanR" == "0" ]]; then
+            vanR_base="Not Covered"
+        else
+            vanR_base=$(bcftools query -f '%ALT' -r FN545816.1:1794733 "${OUTPUT_DIR}${SRA_ID}_variants.vcf.gz")
+        fi
+
         # Write to CSV with coverage check
-        echo "${SRA_ID},${gyrA_base:-N},${coverage_at_gyrA:-0},${nimB_base:-N},${coverage_at_nimB:-0}" >> "$CSV_FILE"
+        echo "${SRA_ID},${gyrA_base:-N},${coverage_at_gyrA:-0},${rpoB_base:-N},${coverage_at_rpoB:-0},${nimB_base:-N},${coverage_at_nimB:-0},${vanR_base:-N},${coverage_at_vanR:-0}" >> "$CSV_FILE"
     else
         echo "BAM file for $SRA_ID not found, skipping."
     fi
